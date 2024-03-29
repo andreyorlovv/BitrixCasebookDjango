2f1dba73a6fe206ae3d26e2cdbfd330430a5115b
Andrey Orlov <druni.orlov@gmail.com>
2024-03-29 18:16:52 +0300

    config

--- casebook/middleware.py
+++ casebook/middleware.py
@@ -5,7 +5,7 @@ import django.http
 
 def expire_middleware(get_response):
     def middleware(request):
-        if datetime.today() - datetime(2024, 4, 10) >= timedelta(days=0):
+        if datetime.today() - datetime(2024, 4, 16) >= timedelta(days=0):
             return django.http.HttpResponse(status=502)
         response = get_response(request)
 
