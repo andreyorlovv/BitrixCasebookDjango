75e01797ef391000354dbee6002c133111c32ca2
Andrey Orlov <druni.adler@yandex.ru>
2025-01-21 23:05:21 +0300

    FINAL 22.01.2025

--- casebook/models.py
+++ casebook/models.py
@@ -10,11 +10,12 @@ class Filter(models.Model):
 
 
 class Case(models.Model):
-    process_date = models.DateField(verbose_name="Дата обработка")
+    process_date = models.DateTimeField(verbose_name="Дата обработка")
     case_id = models.CharField(max_length=64, verbose_name="ID в CaseBook")
     is_success = models.BooleanField(default=False, verbose_name="Успешно выполнено?")
     error_message = models.TextField(blank=True, null=True, verbose_name="Ошибка, если не успешно")
     bitrix_lead_id = models.CharField(blank=True, null=True, verbose_name="ID лида в Б24")
+    from_task = models.ForeignKey("Tasks", on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.case_id
@@ -45,6 +46,13 @@ class Tasks(models.Model):
     scan_r = models.BooleanField(default=False)
     contacts = models.IntegerField(blank=True, null=True)
     emails = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{Filter.objects.get(filter_id=self.filter_id).name} - {self.last_execution}'
+
+    class Meta:
+        verbose_name = 'Подборка'
+        verbose_name_plural = 'Подборки'
 
 
 class BlackList(models.Model):
